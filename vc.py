c3dd3abe2bc5201363e75217a5992176b8f20789
zt#7380 <98675255+x8g@users.noreply.github.com>
2022-07-09 05:45:06 -0400

    fixed rate limit status code again again.

--- vc.py
+++ vc.py
@@ -18,6 +18,7 @@ def check():
     global reqsent
     while True:
         r = requests.get(f'https://discord.com/api/v9/invites/{vanity}')
+        print(r.text)
         if 'vanity_url_code' in (r.text):
             reqsent +=1
             print(Fore.RED + "invite link taken: " + vanity + " | Requests Sent: " +str(reqsent))
@@ -26,7 +27,7 @@ def check():
             reqsent +=1
             print(Fore.GREEN + "invite link claimable or termed: "+ vanity + " | Requests Sent: " +str(reqsent))
             os.system(f'title checking: ' + vanity + ' ^| Status: avail/termed' + ' ^| github/x8g')
-        if 'Error 1015' in (r.text):
+        if 'Access denied' in (r.text):
             print(Fore.RED + f"Rate limited please wait")
         if 'retry_after' in (r.text):
             print(Fore.RED + f"Rate limited please wait.")
