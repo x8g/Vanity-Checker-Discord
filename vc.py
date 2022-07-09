084b440e7c3eec60efa0f0e8c901167c0dda4131
zt#7380 <98675255+x8g@users.noreply.github.com>
2022-07-09 06:03:48 -0400

    added proxy usage!

--- vc.py
+++ vc.py
@@ -5,7 +5,12 @@
 import requests, os, threading, random
 from colorama import Fore
 
-
+ips = requests.get("https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt").text.splitlines()
+def proxys():
+    global proxyss
+    threading.Timer(3.05, proxys).start()
+    proxyss = random.choice(ips)
+# proxys!
 vanity = input(f"What vanity do you want to check?: ")
 threads = input(f"Threads?: ")
 os.system(f'title Vanity Checker ^| Threads: ' + threads)
@@ -16,8 +21,9 @@ reqsent = 0
 # ---
 def check():
     global reqsent
+    global proxyss
     while True:
-        r = requests.get(f'https://discord.com/api/v9/invites/{vanity}')
+        r = requests.get(f'https://discord.com/api/v9/invites/{vanity}', proxies={"http":proxyss})
         if 'vanity_url_code' in (r.text):
             reqsent +=1
             print(Fore.RED + "invite link taken: " + vanity + " | Requests Sent: " +str(reqsent))
@@ -30,6 +36,8 @@ def check():
             print(Fore.RED + f"Cloudfare has blocked this IP.")
         if 'retry_after' in (r.text):
             print(Fore.RED + f"Rate limited please wait.")
+proxys()
+
 while True:
         if threading.active_count() < int(threads):
             threading.Thread(target=check).start()
