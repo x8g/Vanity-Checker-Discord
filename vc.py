f0fd5d4e6e62ddda9c41b0a916d3a3f0471ff5e9
zt#7380 <98675255+x8g@users.noreply.github.com>
2022-07-08 22:07:46 -0400

    fixed whole code and made things look better

--- vc.py
+++ vc.py
@@ -4,40 +4,29 @@
 
 import requests, os, threading, random
 from colorama import Fore
-proxs = {
-  "600",
-  "700",
-  "800",
-  "900",
-  "1000",
-}
 zt = Fore.MAGENTA
 # ---
 vanity = input(f"{zt}what vanity do you want to check?: ")
 threads = input(f"{zt}Threads?: ")
-proxytype = input(f"{zt}Proxy Type?: ")
-# ---
-prox = requests.get(f"https://api.proxyscrape.com/v2/?request=getproxies&protocol={proxytype}&timeout={random.choice(list(proxs))}&country=all").text.splitlines()
 # ---
 os.system(f'title Vanity Checker ^| Threads: ' + threads)
-proxysused = 0
-proxyss = []
-def proxys():
-    lol = random.choice(prox)
+reqsent = 0
+url = f'https://discord.com/api/v9/invites/{vanity}'
+
     
+
 # ---
 def check():
-    global proxysused
-    global proxys
+    global reqsent
     while True:
-        r = requests.get(f"https://discord.com/api/v9/invites/{vanity}")
+        r = requests.get(url)
         if 'vanity_url_code' in (r.text):
-            proxysused +=1
-            print(Fore.RED + "invite link taken: " + vanity + " | Proxys Used: " +str(proxysused))
+            reqsent +=1
+            print(Fore.RED + "invite link taken: " + vanity + " | Requests Sent: " +str(reqsent))
             os.system(f'title checking: ' + vanity + ' ^| Status: taken' + ' ^| github/x8g')
         if 'Unknown Invite' in (r.text):
-            proxysused +=1
-            print(Fore.GREEN + "invite link claimable or termed: "+ vanity + " | Proxys Used: " +str(proxysused))
+            reqsent +=1
+            print(Fore.GREEN + "invite link claimable or termed: "+ vanity + " | Requests Sent: " +str(reqsent))
             os.system(f'title checking: ' + vanity + ' ^| Status: avail/termed' + ' ^| github/x8g')
         if 'retry_after' in (r.text):
             print(Fore.RED + f"Rate limited please wait.")
